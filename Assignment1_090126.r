22e487af88b7daac03982bd4e213f1584e4ea086
Sanjana Venkatesh <venkas17@mcmaster.ca>
2026-01-08 11:50:34 -0500

    Update Assignment1_090126.r

--- Assignment1_090126.r
+++ Assignment1_090126.r
@@ -1,16 +1,8 @@
 library(tidyverse)
 library(ggthemes)
 library(readxl)
-theme_set(theme_linedraw() %+replace%
-            theme(panel.grid.major = element_blank(), 
-                  panel.grid.minor = element_blank(),
-                  text = element_text(face = "bold"),
-                  legend.background = element_rect(size = 0.5, colour = "black"),
-                  strip.background = element_rect(
-                    color="black", fill="gray88", size = 0.5),
-                  strip.text.x = element_text (color = "black"),
-                  strip.text.y = element_text (color = "black")))
 
+# Data used is in the branch "CS_data"
 df1 <- read_excel("CS_data.xlsx")
 
 # Getting the average of triplicate data
