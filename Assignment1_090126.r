a5b21f37498cd726ab5e4779b959b78a2f799dae
Sanjana Venkatesh <venkas17@mcmaster.ca>
2026-01-09 10:45:03 -0500

    Update Assignment1_090126.r

--- Assignment1_090126.r
+++ Assignment1_090126.r
@@ -1,15 +1,9 @@
 library(tidyverse)
 library(ggthemes)
 library(readxl)
-theme_set(theme_linedraw() %+replace%
-            theme(panel.grid.major = element_blank(), 
-                  panel.grid.minor = element_blank(),
-                  text = element_text(face = "bold"),
-                  legend.background = element_rect(size = 0.5, colour = "black"),
-                  strip.background = element_rect(
-                    color="black", fill="gray88", size = 0.5),
-                  strip.text.x = element_text (color = "black"),
-                  strip.text.y = element_text (color = "black")))
+library(glmmTMB)
+library(performance)
+library(car)
 
 df1 <- read_excel("CS_data.xlsx")
 
@@ -17,3 +11,13 @@ df1 <- read_excel("CS_data.xlsx")
 df_CS <- df1 %>%
   group_by(across(1:4)) %>%
   summarise(mean_CS = mean(CS_activity, na.rm = TRUE))
+
+df_CS[,c(1:4)]<- lapply(df_CS[,c(1:4)], as.factor)
+
+# Linear mixed model
+mod1 <- glmmTMB(mean_CS ~ (AT + AS + TT)^3 + (1|genotype), data = df_CS)
+check_model(mod1)
+summary(mod1)
+
+# Run ANOVA
+Anova(mod1, type = "II")
